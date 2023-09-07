4b7033d8696711b7499797a87d961d62120a0ddc
Your Name <you@example.com>
2023-09-06 19:46:22 -0700

    first message

---
+++ src/app/app.component.ts
@@ -0,0 +1,97 @@
+import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'tic-tac-toe';
+
+  winMessage: string = '';
+  isCross = false;
+  isDraw = false;
+  itemArray : string[] = new Array(9).fill('empty');
+
+  constructor(private toastr : ToastrService) {}
+
+  handleClick = (itemNumber:number)=> {
+      if (this.winMessage.length > 0) {
+        this.toastr.success(this.winMessage)
+      }
+      if(this.itemArray[itemNumber] === 'empty'){
+        this.itemArray[itemNumber] = this.isCross ? 'Cross' : 'Circle'
+        this.isCross =!this.isCross
+        this.checkIsWinner();
+      } else{
+        this.toastr.info('Already filled')
+      }
+      if (this.isDraw) {
+        this.toastr.warning('The game ended in a draw');
+      }
+  }
+
+  checkIsWinner = () => {
+    //  checking  winner of the game
+    if (
+      this.itemArray[0] === this.itemArray[1] &&
+      this.itemArray[1] === this.itemArray[2] &&
+      this.itemArray[0] !== 'empty'
+    ) {
+      this.winMessage = `${this.itemArray[0]} won`;
+    } else if (
+      this.itemArray[3] !== 'empty' &&
+      this.itemArray[3] === this.itemArray[4] &&
+      this.itemArray[4] === this.itemArray[5]
+    ) {
+      this.winMessage = `${this.itemArray[3]} won`;
+    } else if (
+      this.itemArray[6] !== 'empty' &&
+      this.itemArray[6] === this.itemArray[7] &&
+      this.itemArray[7] === this.itemArray[8]
+    ) {
+      this.winMessage = `${this.itemArray[6]} won`;
+    } else if (
+      this.itemArray[0] !== 'empty' &&
+      this.itemArray[0] === this.itemArray[3] &&
+      this.itemArray[3] === this.itemArray[6]
+    ) {
+      this.winMessage = `${this.itemArray[0]} won`;
+    } else if (
+      this.itemArray[1] !== 'empty' &&
+      this.itemArray[1] === this.itemArray[4] &&
+      this.itemArray[4] === this.itemArray[7]
+    ) {
+      this.winMessage = `${this.itemArray[1]} won`;
+    } else if (
+      this.itemArray[2] !== 'empty' &&
+      this.itemArray[2] === this.itemArray[5] &&
+      this.itemArray[5] === this.itemArray[8]
+    ) {
+      this.winMessage = `${this.itemArray[2]} won`;
+    } else if (
+      this.itemArray[0] !== 'empty' &&
+      this.itemArray[0] === this.itemArray[4] &&
+      this.itemArray[4] === this.itemArray[8]
+    ) {
+      this.winMessage = `${this.itemArray[0]} won`;
+    } else if (
+      this.itemArray[2] !== 'empty' &&
+      this.itemArray[2] === this.itemArray[4] &&
+      this.itemArray[4] === this.itemArray[6]
+    ) {
+      this.winMessage = `${this.itemArray[2]} won`;
+    }
+    if (!this.winMessage && !this.itemArray.includes('empty')) {
+      this.winMessage = 'It\'s a draw!';
+      this.isDraw = true;
+    }
+  };
+
+  reloadGame = () => {
+    this.winMessage = '';
+    this.isCross = false;
+    this.itemArray = new Array(9).fill('empty');
+  };
+}
